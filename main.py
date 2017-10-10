32dcc3ad3d19bfb06a5861c61a36284a9f8b3ece
Taruna <tarunajindal1998@gmail.com>
2017-10-10 23:06:20 +0530

    menu added

--- main.py
+++ main.py
@@ -1,10 +1,68 @@
-import spy_details
+from spy_details import spy_name, spy_salutation, spy_rating, spy_age
+
+
+STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.']
+
 print 'Let\'s get started'
-question = "Continue as " + spy_details.spy_salutation + " " + spy_details.spy_name + "(Y/N)?"
+question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"
 existing = raw_input(question)
+
+
+def add_status(current_status_message):
+
+    updated_status_message = None
+
+    if current_status_message != None:
+      print 'Your current status message is %s \n' % (current_status_message)
+    else:
+      print 'You don\'t have any status message currently \n'
+
+    default = raw_input("Do you want to select from the older status (y/n)? ")
+    if default.upper() == "N":
+          new_status_message = raw_input("What status message do you want to set?")
+
+          if len(new_status_message) > 0:
+              updated_status_message = new_status_message
+              STATUS_MESSAGES.append(updated_status_message)
+    elif default.upper() == "Y":
+          item_position = 1
+          for message in STATUS_MESSAGES:
+              print '%d. %s' % (item_position, message)
+              item_position = item_position + 1
+          message_selection = raw_input("\nChoose from the above messages ")
+          if len(STATUS_MESSAGES) >= message_selection:
+             updated_status_message = STATUS_MESSAGES[message_selection - 1]
+    else:
+          print 'The option you chose is not valid! Press either y or n.'
+
+    if updated_status_message:
+        print 'Your updated status message is: %s' % (updated_status_message)
+    else:
+        print 'You did not update your status message'
+
+    return updated_status_message
+
+
+
+def start_chat(spy_name,spy_age, spy_rating):
+  show_menu = True
+  current_status_message = None
+
+  while show_menu:
+    menu_choices = "What do you want to do? \n1. Add a status update \n2. Close Application"
+    menu_choice = raw_input(menu_choices)
+
+    if menu_choice == '1':
+      current_status_message = add_status(current_status_message)
+
+    elif menu_choice == '2':
+      show_menu = False
+
+
 
 if (existing == "Y"):
     print 'ok'
+    start_chat(spy_name,spy_age,spy_rating)
 #Continue with the default user/details imported from the spy_details file.
 else:
     spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
@@ -33,5 +91,7 @@ else:
 
         print "Authentication complete. Welcome %s of age: %d and rating %.2f Proud to have you onboard" % (
         spy_name, spy_age, spy_rating)
+
+        start_chat(spy_name, spy_age, spy_rating)
     else:
         print "A spy needs to have a valid name. Try again please."
